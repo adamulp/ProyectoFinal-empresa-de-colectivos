b2b86e804cc17b887308876d2b2117e7b4009cc1
adamulp <adamgoestoargentina@gmail.com>
2024-06-01 21:31:40 -0300

    Update Horario.java

--- EmpresaDeColectivo_Grupo3/src/EmpresaDeColectivo/Entidades/Horario.java
+++ EmpresaDeColectivo_Grupo3/src/EmpresaDeColectivo/Entidades/Horario.java
@@ -1,10 +1,75 @@
 package EmpresaDeColectivo.Entidades;
 
+import java.time.LocalTime;
+
 /**
  *
  * @author Allendez, Casalengo, Hollmann, Rigg
  */
 public class Horario {
+    private int idHorario;
+    private int idRuta;
+    private LocalTime horaSalida;
+    private LocalTime horaLlegada;
+    private boolean estado;
+
+    public Horario() {
+    }
+
+    public Horario(int idRuta, LocalTime horaSalida, LocalTime horaLlegada, boolean estado) {
+        this.idRuta = idRuta;
+        this.horaSalida = horaSalida;
+        this.horaLlegada = horaLlegada;
+        this.estado = estado;
+    }
+
+    public Horario(int idRuta, LocalTime horaSalida, LocalTime horaLlegada) {
+        this(idRuta, horaSalida, horaLlegada, true);
+    }
+
+    public int getIdHorario() {
+        return idHorario;
+    }
+
+    public int getIdRuta() {
+        return idRuta;
+    }
+
+    public LocalTime getHoraSalida() {
+        return horaSalida;
+    }
+
+    public LocalTime getHoraLlegada() {
+        return horaLlegada;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setIdHorario(int idHorario) {
+        this.idHorario = idHorario;
+    }
+
+    public void setIdRuta(int idRuta) {
+        this.idRuta = idRuta;
+    }
+
+    public void setHoraSalida(LocalTime horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public void setHoraLlegada(LocalTime horaLlegada) {
+        this.horaLlegada = horaLlegada;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
     
 }
 /*
