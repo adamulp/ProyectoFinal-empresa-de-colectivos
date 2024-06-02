18ef4884dd8bec3f9336eb0123907a5134ca1141
adamulp <adamgoestoargentina@gmail.com>
2024-06-01 21:26:46 -0300

    Update Ruta.java

--- EmpresaDeColectivo_Grupo3/src/EmpresaDeColectivo/Entidades/Ruta.java
+++ EmpresaDeColectivo_Grupo3/src/EmpresaDeColectivo/Entidades/Ruta.java
@@ -1,10 +1,77 @@
 package EmpresaDeColectivo.Entidades;
 
+import java.time.Duration;
+
+
 /**
  *
  * @author Allendez, Casalengo, Hollmann, Rigg
  */
 public class Ruta {
+    private int idRuta;
+    private String origen;
+    private String destino;
+    private Duration duracionEstimada;
+    private boolean estado;
+
+    public Ruta() {
+    }
+
+    public Ruta(String origen, String destino, Duration duracionEstimada, boolean estado) {
+        this.origen = origen;
+        this.destino = destino;
+        this.duracionEstimada = duracionEstimada;
+        this.estado = estado;
+    }
+
+    public Ruta(String origen, String destino, Duration duracionEstimada) {
+        this(origen, destino, duracionEstimada, true);
+    }
+
+    public int getIdRuta() {
+        return idRuta;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public Duration getDuracionEstimada() {
+        return duracionEstimada;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setIdRuta(int idRuta) {
+        this.idRuta = idRuta;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setDuracionEstimada(Duration duracionEstimada) {
+        this.duracionEstimada = duracionEstimada;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
+    
     
 }
 /*
