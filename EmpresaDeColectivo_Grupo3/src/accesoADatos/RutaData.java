3d0b6606a526ba74c151dbaea48e12367469b65d
adamulp <adamgoestoargentina@gmail.com>
2024-06-06 20:42:55 -0300

    TryBlock, pS

--- EmpresaDeColectivo_Grupo3/src/accesoADatos/RutaData.java
+++ EmpresaDeColectivo_Grupo3/src/accesoADatos/RutaData.java
@@ -1,5 +1,6 @@
 package accesoADatos;
 
+import EmpresaDeColectivo.Entidades.Ruta;
 import java.sql.Connection;
 import java.sql.Connection;
 import java.sql.Date;
@@ -9,6 +10,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -20,5 +23,18 @@ public class RutaData {
 
     public RutaData() {
         con = Conexion.getConexion();
-    } 
+    }
+    
+    public void guardarRuta(Ruta ruta){
+        String sql = " INSERT INTO Rutas (ID_Ruta, Origin, Destino, "
+                + " Duracion_Estimada, Estado "
+                + "VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+        } catch (SQLException ex) {
+            Logger.getLogger(RutaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
