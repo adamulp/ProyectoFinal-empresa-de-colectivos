c3f21fda01955e38dd868aee32c7afcf34cdbe27
adamulp <adamgoestoargentina@gmail.com>
2024-06-06 20:28:48 -0300

    Update RutaData.java

--- EmpresaDeColectivo_Grupo3/src/accesoADatos/RutaData.java
+++ EmpresaDeColectivo_Grupo3/src/accesoADatos/RutaData.java
@@ -1,9 +1,24 @@
 package accesoADatos;
 
+import java.sql.Connection;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author adam, enzo, alexis, nicolas
  */
 public class RutaData {
-    
+    private Connection con = null;
+
+    public RutaData() {
+        con = Conexion.getConexion();
+    } 
 }
